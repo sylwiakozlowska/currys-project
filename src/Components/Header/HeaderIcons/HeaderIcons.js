5396b39d7f88cb55bf839a56b1b297d3610ad9d1
Sylwia Kozlowska <kozlowska.sylwia90@gmail.com>
2022-11-07 17:15:14 +0000

    add header icons comp and add new icons to icons folder

---
+++ src/Components/Header/HeaderIcons/HeaderIcons.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../HeaderIcons/HeaderIcons.scss";
+
+export const HeaderIcons = () => {
+  return (
+    <div className="header-icons-component">
+      <div className="header-icons-content">
+        <div className="header-icons">
+            <a href="#">
+              <i className="glyph location-icon"></i>
+              <p>Stores</p>
+            </a>
+            <a href="#">
+              <i className="glyph account-icon"></i>
+              <p className="sign">Account</p>
+            </a>
+            <a href="#">
+              <i className="glyph basket-icon"></i>
+              <p>Basket</p>
+            </a>
+        </div>
+      </div>
+    </div>
+  );
+};
