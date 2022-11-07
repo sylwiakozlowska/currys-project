e83498df8cba8bb45b4fb0e37574507288c15ce3
Sylwia Kozlowska <kozlowska.sylwia90@gmail.com>
2022-11-07 17:15:57 +0000

    add header icon comp to the header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../styles.scss";
 import logo from "../../assets/logo.svg";
+import { HeaderIcons } from "./HeaderIcons/HeaderIcons";
 
 export const Header = () => {
   return (
@@ -11,9 +12,11 @@ export const Header = () => {
       <div className="header-content-wrapper">
         <div className="header-content">
           <div className="header-search-wrapper">search</div>
-          <div className="header-icons-wrapper">icons</div>
+          <div className="header-icons-wrapper">
+            <HeaderIcons />
+          </div>
         </div>
-        <div className="header-menu-nav-wrapper">menu</div>{" "}
+        <div className="header-menu-nav-wrapper">menu</div>
       </div>
     </section>
   );
