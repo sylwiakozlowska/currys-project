76b2e124223a59ab9357c569842471bdebbdce6e
Sylwia Kozlowska <kozlowska.sylwia90@gmail.com>
2022-11-11 14:08:43 +0000

    add a carousel to the component

--- src/Components/Card/CardDeal.js
+++ src/Components/Card/CardDeal.js
@@ -2,7 +2,16 @@ import React from "react";
 import "../../styles.scss";
 import { useSelector } from "react-redux";
 import { selectCardDeal } from "../../Store/features/cardDeal/cardDealSlice";
+import Slider from "infinite-react-carousel";
 
+const settings = {
+  autoplay: true,
+  autoplaySpeed: 8000,
+  wheelScroll: 3,
+  // dots: true,
+  centerPadding: 90,
+  arrows: true,
+};
 export const CardDeal = () => {
   const { dealOptions, dealSaveOptions } = useSelector(selectCardDeal);
   const dealOptionsList = dealOptions.map(({ id, src, alt }) => {
@@ -22,7 +31,11 @@ export const CardDeal = () => {
   return (
     <div className="card-deal-component">
       <div className="card-deal-content">{dealOptionsList}</div>
-      <div className="card-deal-save-content">{dealSaveOptionsList}</div>
+      <div className="card-deal-save-content">
+        <Slider className="carousel" {...settings}>
+          {dealSaveOptionsList}
+        </Slider>
+      </div>
     </div>
   );
 };
