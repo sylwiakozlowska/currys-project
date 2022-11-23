fb12d157e19c246d2a3a1e7f375ead4aae47083d
Sylwia Kozlowska <kozlowska.sylwia90@gmail.com>
2022-11-23 10:54:49 +0000

    add a dispach action

--- src/Components/MenuTablet/MenuNavTablet/MenuNavTablet.js
+++ src/Components/MenuTablet/MenuNavTablet/MenuNavTablet.js
@@ -6,25 +6,26 @@ import { MenuTabletList } from "../MenuTabletList";
 import { useDispatch } from "react-redux";
 import { showMenu } from "../../../Store/features/app/appSlice";
 
-
-
 export const MenuNavTablet = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   return (
-    <nav className="menu-nav-tablet-component">
-      <div className="menu-nav-tablet-content">
-        <div className="menu-nav-tablet-close-button">
-          <button>
-            <i className="glyph close-icon"  onClick={() => {
-            dispatch(showMenu());
-          }}></i>
-          </button>
+      <nav className="menu-nav-tablet-component">
+        <div className="menu-nav-tablet-content">
+          <div className="menu-nav-tablet-close-button">
+            <button>
+              <i
+                className="glyph close-icon"
+                onClick={() => {
+                  dispatch(showMenu());
+                }}
+              ></i>
+            </button>
+          </div>
+          <h1 className="menu-nav-tablet-title">Shop by category</h1>
+          <MenuTabletList />
+          <HelpLinks />
         </div>
-        <h1 className="menu-nav-tablet-title">Shop by category</h1>
-        <MenuTabletList />
-        <HelpLinks />
-      </div>
-    </nav>
+      </nav>
   );
 };
