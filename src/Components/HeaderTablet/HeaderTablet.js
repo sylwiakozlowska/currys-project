b20e11e062b649b2dbd2b35d6bacd1efcb9fc736
Sylwia Kozlowska <kozlowska.sylwia90@gmail.com>
2022-11-29 13:57:41 +0000

    add a non pure wrapper around header-tablet

--- src/Components/HeaderTablet/HeaderTablet.js
+++ src/Components/HeaderTablet/HeaderTablet.js
@@ -4,16 +4,18 @@ import logo from "../../assets/logo.svg";
 import { HeaderIcons } from "../Header/HeaderIcons/HeaderIcons";
 import { useDispatch } from "react-redux";
 import { showMenu } from "../../Store/features/app/appSlice";
-
+import { SearchBarWrapper } from "../SearchBar/SearchBarWrapper";
 
 export const HeaderTablet = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   return (
     <section className="header-tablet-component">
       <div className="header-tablet-logo-wrapper">
         <img src={logo} />
       </div>
-      <div className="header-tablet-search-wrapper">search</div>
+      <div className="header-tablet-search-wrapper">
+        <SearchBarWrapper />
+      </div>
       <div className="header-tablet-icons-wrapper">
         <HeaderIcons />
         <a
