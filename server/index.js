87169ded0de41c7fd6a2b4eff5d0250a9881dab5
Sylwia Kozlowska <kozlowska.sylwia90@gmail.com>
2023-03-01 11:28:41 +0100

    import products to the server

--- server/index.js
+++ server/index.js
@@ -3,13 +3,13 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-const products = require('./products.js');
-
+const summaryProducts = require("./summaryProducts.js");
+const allProducts = require("./allProducts.js");
 
 app.get("/delayed-response", (req, res) => {
   setTimeout(() => {
     res.json("This response was delayed by 3 seconds.");
-  }, 8000);
+  }, 2000);
 });
 
 app.get("/api", (req, res) => {
@@ -17,9 +17,28 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/products", (req, res) => {
-  console.log(products, "products");
-  res.json(products);
+  console.log(summaryProducts, "summaryProducts");
+  res.json(summaryProducts);
 });
+app.get("/product", (req, res) => {
+  console.log(allProducts, "allProducts");
+  res.json(allProducts);
+});
+
+// app.get("/product/:id", (req, res) => {
+//   // const { id } = req.params;
+//   const { payload } = req.query;
+//   const { detailedProducts } = allProducts;
+//   const detailedProduct = detailedProducts.filter(
+//     ({ id, text, price, rating, imageUrl }) =>
+//       new RegExp( payload,"i").test(text, id)
+//   );
+//   res.json(detailedProduct);
+// });
+// app.get("/product/:id", (req, res) => {
+//   console.log(allProducts, "allProducts");
+//   res.json(allProducts);
+// });
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
