e2d2c9ba1a2fac62161381adfcf24e9f39b06dbf
Sylwia Kozlowska <kozlowska.sylwia90@gmail.com>
2023-03-01 11:39:20 +0100

    add product summary tablet and change names

--- src/Components/FiltersWithProducts/FiltersWithProducts.js
+++ src/Components/FiltersWithProducts/FiltersWithProducts.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { FilterComponent } from "../FilterComponent/FilterComponent";
 // import { intersectionWith, remove } from "lodash";
-import { NonPureProductSummary } from "../ProductSummary/NonPureProductSummary/NonPureProductSummary";
-
+import { ProductSummaryWrapper } from "../ProductSummaryWrapper/ProductSummaryWrapper";
+import { ProductSummaryWrapperTablet } from "../ProductSummaryWrapperTablet/ProductSummaryWrapperTablet";
 
 export const FiltersWithProducts = () => {
-  // const productSummaries = products.map((product) => {
-  //   return <ProductSummary product={product} onCompare={onCompare} />;
-  // });
   return (
     <div className="filters-with-products-component">
       <div className="filters-with-products-content">
         <FilterComponent />
-        <NonPureProductSummary />
+        <ProductSummaryWrapper />
+      </div>
+      <div className="filters-with-products-content-tablet">
+        <ProductSummaryWrapperTablet />
       </div>
     </div>
   );
