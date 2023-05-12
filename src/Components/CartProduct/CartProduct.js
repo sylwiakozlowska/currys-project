66188507ccb7c11d1ff3b7a3a50238923c35046a
Sylwia Kozlowska <kozlowska.sylwia90@gmail.com>
2023-05-12 11:26:30 +0100

    add cart product

---
+++ src/Components/CartProduct/CartProduct.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { CardComponent } from "../CardComponent/CardComponent";
+import { DeliveryOptions } from "../DeliveryOptions/DeliveryOptions";
+export const CartProduct = ({ cartProduct, onClick, onChange }) => {
+  const { title, count, productImageUrl, productPrice, deliveries } =
+    cartProduct;
+
+  const totalPrice = count * Number(productPrice).toFixed(2);
+  return (
+    <CardComponent>
+      <div className="cart-product-component">
+        <div className="columns">
+        <div className="column is-one-third">
+          <img src={productImageUrl} /></div>
+          <div className="column">
+            <h1 className="cart-product-title title is-1">{title}</h1>
+            <div className="cart-product-details">
+              <div className="select-quantity">
+                <div>Quantity</div>
+                <div className="select">
+                  <select
+                    className="show-quantity"
+                    value={count}
+                    onChange={(e) => onChange(e.target.value, cartProduct)}
+                  >
+                    <option value="1"> 1</option>
+                    <option value="2"> 2</option>
+                    <option value="3"> 3</option>
+                    <option value="4"> 4</option>
+                    <option value="5"> 5</option>
+                  </select>
+                </div>
+              </div>
+              <button onClick={onClick}>Remove item</button>
+              <button>Save for later</button>
+              <div className="price-status">£{totalPrice}</div>
+            </div>
+            <DeliveryOptions deliveries={deliveries} />
+          </div>
+        </div>
+      </div>
+    </CardComponent>
+  );
+};
---
+++ src/Pages/Cart/Cart.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import "../../styles.scss";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  fetchProduct,
+  selectCartProduct,
+  selectStatus,
+} from "../../Store/features/cart/cartProductSlice";
+import {
+  removeFromBasket,
+  setProductCount,
+  selectItemCount
+} from "../../Store/features/cart/cartProductSlice";
+import { CartProduct } from "../../Components/CartProduct/CartProduct";
+// import { ProductImage } from "../../Components/ProductSummary/ProductImage/ProductImage";
+
+export const Cart = () => {
+  // const [quantity, setQuantity] = useState(0);
+
+  const products = useSelector(selectCartProduct);
+  console.log("products", products);
+  const dispatch = useDispatch();
+
+  //add the count to total when the quantity is changed
+  const itemsCount = useSelector(selectItemCount)
+  const basketItems = `(${itemsCount} ${itemsCount === 1 ? "item" : "items"})`;
+
+  const onChangeQuantity = (count, { id }) => {
+    dispatch(setProductCount({ count, id }));
+  };
+  const onClickRemoveItem = (cartProduct) => {
+    console.log(cartProduct);
+    dispatch(removeFromBasket(cartProduct));
+  };
+
+  const cartProducts = products.map((cartProduct) => {
+    return (
+      <CartProduct
+        cartProduct={cartProduct}
+        onClick={onClickRemoveItem}
+        onChange={onChangeQuantity}
+      />
+    );
+  });
+
+  return (
+    <div className="cart-component">
+      <h1 className="product-title title is-1">
+        Your basket {basketItems}
+      </h1>
+      {cartProducts}
+    </div>
+  );
+};
